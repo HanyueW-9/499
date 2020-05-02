186cc526cd950dba74c9ee1c5d2bbb02d1393d19
HanyueW-9 <55716153+HanyueW-9@users.noreply.github.com>
2020-05-01 22:30:08 -0700

    Update project_6.py

--- project_6.py
+++ project_6.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
 import gzip
 import codecs
 import os
@@ -13,6 +15,52 @@ from scipy import ndimage
 
 from six.moves import urllib
 
+# Create model of CNN with slim api
+def CNN(inputs, is_training=True):
+    batch_norm_params = {'is_training': is_training, 'decay': 0.9, 'updates_collections': None}
+    with slim.arg_scope([slim.conv2d, slim.fully_connected],
+                        normalizer_fn=slim.batch_norm,
+                        normalizer_params=batch_norm_params):
+        x = tf.reshape(inputs, [-1, 16, 16, 1])
+
+        # For slim.conv2d, default argument values are like
+        # normalizer_fn = None, normalizer_params = None, <== slim.arg_scope changes these arguments
+        # padding='SAME', activation_fn=nn.relu,
+        # weights_initializer = initializers.xavier_initializer(),
+        # biases_initializer = init_ops.zeros_initializer,
+        net = slim.conv2d(x, 32, [3, 3], scope='conv1')
+        net = slim.max_pool2d(net, [2, 2], scope='pool1')
+        net = slim.conv2d(net, 64, [3, 3], scope='conv2')
+        net = slim.max_pool2d(net, [2, 2], scope='pool2')
+        net = slim.dropout(net, is_training=is_training, scope='dropout2', keep_prob=0.75)  # 0.5 by default
+        net = slim.flatten(net, scope='flatten3')
+
+        # For slim.fully_connected, default argument values are like
+        # activation_fn = nn.relu,
+        # normalizer_fn = None, normalizer_params = None, <== slim.arg_scope changes these arguments
+        # weights_initializer = initializers.xavier_initializer(),
+        # biases_initializer = init_ops.zeros_initializer,
+        net = slim.fully_connected(net, 128, scope='fc3')
+        net = slim.dropout(net, is_training=is_training, scope='dropout3')  # 0.5 by default
+        outputs = slim.fully_connected(net, 10, activation_fn=None, normalizer_fn=None, scope='fco')
+    return outputs
+
+# Create model of FCN with slim api
+def FCN(inputs, is_training=True):
+    batch_norm_params = {'is_training': is_training, 'decay': 0.9, 'updates_collections': None}
+    with slim.arg_scope([slim.conv2d, slim.fully_connected],
+                        normalizer_fn=slim.batch_norm,
+                        normalizer_params=batch_norm_params):
+        x = tf.reshape(inputs, [-1, 16, 16, 1])
+        net = slim.flatten(x, scope='flatten')
+        net = slim.fully_connected(net, 270, scope='fc1')
+       
+        net = slim.fully_connected(net, 270, scope='fc2')
+       
+        net = slim.fully_connected(net, 128, scope='fc3')
+        
+        outputs = slim.fully_connected(net, 10, activation_fn=None, normalizer_fn=None, scope='fco')
+    return outputs
 
 SOURCE_URL = 'http://yann.lecun.com/exdb/mnist/'
 DATA_DIRECTORY = "data"
